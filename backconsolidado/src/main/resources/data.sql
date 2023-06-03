38e2c8d824341eaae6286278e93eaa2d136687a6
DESKTOP-D5CNHSR\mauri <mauriciogm123m@gmail.com>
2023-06-03 10:55:56 -0500

    Back-Consolidado-EntidadSM

---
+++ backconsolidado/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO TEAMMEMBERS (name, lastName, rol) VALUES
+  ('Lokesh', 'Gupta', 'abc@gmail.com'),
+  ('Deja', 'Vu', 'xyz@email.com'),
+  ('Caption', 'America', 'cap@marvel.com');
+  
+  INSERT INTO PROJECTS (ProjectName, codeID, descripcion) VALUES
+  ('Lokesh', 'Gupta', 'abc@gmail.com'),
+  ('Deja', 'Vu', 'xyz@email.com'),
+  ('Caption', 'America', 'cap@marvel.com');
