5f022f9de2c3ce6b45477a4027a9d7beced424ab
DESKTOP-D5CNHSR\mauri <mauriciogm123m@gmail.com>
2023-06-09 16:12:17 -0500

    BackMeloTodos

--- backconsolidado/src/main/resources/data.sql
+++ backconsolidado/src/main/resources/data.sql
@@ -15,3 +15,8 @@ INSERT INTO TEAMMEMBERS (name, lastName, rol) VALUES
   ( 'Gupta', 'Software'),
   ( 'Vu', 'software'),
   ( 'America', 'Arquitectura');
+  
+  INSERT INTO TASKS (taskName, taskDescription, startDate, finishDate) VALUES
+  ('Lokesh', 'Gupta', '01-01-2000','01-01-2001'),
+  ('Deja', 'Vu', '02-02-2002','02-02-2003'),
+  ('Caption', 'America', '03-03-2004','04-04-2005');
